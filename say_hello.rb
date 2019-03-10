93bea8488267f63b0400729ed4b72e93efdfdbdb
Joshua Williams <j.o-williams@outlook.com>
2019-03-10 12:18:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
-def say_hello (phrase = "Ruby Programmer")
+# def say_hello (phrase = "Ruby Programmer")
   
-  puts "Hello #{phrase}!"
+#   puts "Hello #{phrase}!"
   
-end
+# end
 
 say_hello("Gabriela")
 say_hello
